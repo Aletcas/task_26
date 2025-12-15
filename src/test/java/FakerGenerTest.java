6a7a59fff4f01f3eb057c43860e4ef59443eaf02
Aletca <aletca@gmail.com>
2025-12-15 22:41:53 +0300

    task 26

---
+++ src/test/java/FakerGenerTest.java
@@ -0,0 +1,31 @@
+import com.github.javafaker.Faker;
+import com.microsoft.playwright.*;
+import org.junit.jupiter.api.Test;
+
+
+import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
+
+public class FakerGenerTest {
+
+    Faker faker = new Faker();
+
+    @Test
+    void testFakerGenerate() {
+        try (Playwright playwright = Playwright.create();
+             Browser browser = playwright.chromium().launch();
+             BrowserContext context = browser.newContext();
+             Page page = context.newPage()) {
+
+            String expectedName = faker.name().fullName();
+            Locator actualName = page.locator(".large-10.columns");
+
+            page.route("**/dynamic_content", route -> {
+                route.fulfill(new Route.FulfillOptions()
+                        .setBody("<div class='large-10 columns'>" + expectedName + "</div>"));
+            });
+
+            page.navigate("https://the-internet.herokuapp.com/dynamic_content");
+            assertThat(actualName).hasText(expectedName);
+        }
+    }
+}
